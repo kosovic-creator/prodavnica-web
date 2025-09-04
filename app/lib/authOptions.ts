07beaee40134bbafbb93c3b7e7f1b6d8bb290ff0
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-04 07:29:16 +0200

    inir

--- app/lib/authOptions.ts
+++ app/lib/authOptions.ts
@@ -1,5 +1,5 @@
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "./prisma";
+import prisma from "../lib/prisma";
 import bcrypt from "bcryptjs";
 
 const authOptions = {
