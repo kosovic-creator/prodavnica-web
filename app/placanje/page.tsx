efc7e89faf8867280fe31e95290179bd80a3192a
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-08 20:51:31 +0200

    plaćanje2

--- app/placanje/page.tsx
+++ app/placanje/page.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/prefer-as-const */
 'use client';
 import React, { useEffect, useState } from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from '@/components/CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51QzXjhGsYKIy68At6fXHC8XTKOsEwwPcC8M3bkQaaSFFmgSymnndIqk2ZJD8xEtNDWF2TdYPyfd6Ah7j0XYgKT1z005tFoGnFq'); // koristi svoj publishable key
 
 function PlacanjePage() {
   const [clientSecret, setClientSecret] = useState<string>('');
@@ -9,16 +14,28 @@ function PlacanjePage() {
     fetch('/api/payment/create-payment-intent', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ amount: 1000, currency: 'rsd' }), // 1000 RSD
+        body: JSON.stringify({ amount: 100, currency: 'eur' }),
     })
       .then(res => res.json())
-      .then(data => setClientSecret(data.clientSecret));
+        .then(data => {
+            setClientSecret(data.clientSecret);
+            console.log(data.clientSecret); // OVDE dodaj log
+        });
   }, []);
+
+    const options = {
+        clientSecret,
+        appearance: { theme: "stripe" as "stripe" },
+    };
 
   return (
     <div>
       <h2>Plaćanje</h2>
-      {clientSecret && <CheckoutForm clientSecret={clientSecret} />}
+          {clientSecret && (
+              <Elements stripe={stripePromise} options={options}>
+                  <CheckoutForm clientSecret={clientSecret} />
+              </Elements>
+          )}
     </div>
   );
 }
