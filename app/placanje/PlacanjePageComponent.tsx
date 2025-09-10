a75cad42250f32e3441939fe575ebd2bc4526680
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-10 07:52:31 +0200

    1234

--- app/placanje/PlacanjePageComponent.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-import type { FC } from 'react';
-
-const DynamicPlacanjePage = dynamic(() => import('./PlacanjePageComponent'), { ssr: false }) as FC;
-
-export default DynamicPlacanjePage;
