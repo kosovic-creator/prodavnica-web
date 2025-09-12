08b6164bd11457b2e9a5a66ffa9a01b362a2b043
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-12 16:05:12 +0200

    localizam

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,16 @@
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      email: string;
-      name?: string;
-      role?: string;
-    };
-  }
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import NextAuth from "next-auth";
 
+declare module "next-auth" {
   interface User {
     id: string;
     email: string;
     name?: string;
     role?: string;
+  }
+
+  interface Session {
+    user: User;
   }
 }
 
