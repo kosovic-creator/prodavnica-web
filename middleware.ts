44002fab2c33350f70fa5ad23d23e8aa8ccc7adc
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-18 23:01:34 +0200

    midleware dobar

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
   const { pathname } = new URL(request.url);
-  // Proveri da li ruta počinje sa /korisnik
-  if (pathname.startsWith('/korisnici')) {
+  // Proveri da li ruta počinje sa /admin
+  if (pathname === '/admin' || pathname.startsWith('/admin/')) {
     // Pročitaj token iz cookie-ja
     const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
     // Ako nema tokena, preusmeri na login
